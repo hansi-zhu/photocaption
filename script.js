348b966511eb8b53edbd2f1e5b7c014f6f2fe9bb
Hansi Zhu <143524144+hansi-zhu@users.noreply.github.com>
2024-02-21 22:37:14 -0500

    launch!

---
+++ script.js
@@ -0,0 +1,26 @@
+let img;
+let smallPoint, largePoint;
+
+function preload() {
+  img = loadImage('./hanyu.png');
+  image(img, 100, 500, 1046,1400);
+
+}
+
+function setup() {
+  createCanvas(1046, 1400);
+  smallPoint = 4;
+  largePoint = 40;
+  imageMode(CENTER);
+  noStroke();
+  img.loadPixels();
+}
+
+function draw() {
+  let pointillize = map(mouseX, 0, width, smallPoint, largePoint);
+  let x = floor(random(img.width));
+  let y = floor(random(img.height));
+  let pix = img.get(x, y);
+  fill(pix, 128);
+  ellipse(x, y, pointillize, pointillize);
+}
